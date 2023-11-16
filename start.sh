916d27b4edc25eb9821bc84ae6182dd9aac9b465
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2023-11-16 23:59:45 +0100

    Use a script for the start command

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+set -e
+elm make src/Main.elm --output build/main.js 
+rm -f build/main.mjs 
+echo -e 'var global = {};\n' > build/main.mjs 
+sed 's/this..;/global));/' < build/main.js >> build/main.mjs 
+echo -e '\n\nexport const Elm = global.Elm;' >> build/main.mjs 
+echo -e '================================\n' 
+node --trace-warnings index.mjs
