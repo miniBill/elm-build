31b347f8bcc859a32fcbb94b461c7a22282c757f
Leonardo Taglialegne <leonardo@taglialegne.it>
2026-03-02 13:35:42 +0100

    Splitting more

--- src/BuildAction.elm
+++ src/BuildAction.elm
@@ -6,6 +6,7 @@ import BackendTask.Glob as Glob
 import Cache exposing (FileOrDirectory)
 import Cache.Do as Do
 import Cache.Font as Font
+import Cache.Image as Image
 import Elm
 import Elm.Annotation
 import Elm.Arg
@@ -265,8 +266,8 @@ processFile config total index ( path, copyFile ) =
         image : String -> Cache.Monad (Maybe ProcessedFile)
         image originalExtension =
             Cache.do copyFile <| \copied ->
-            Do.pipeThrough "exiftool" [ "-all=", "-", "-o", "-" ] copied <| \stripped ->
-            Do.pipeThrough "identify" [ "-ping", "-format", "%w %h", "-" ] stripped <| \sizeFile ->
+            Cache.do (Image.stripMetadata copied) <| \stripped ->
+            Cache.do (Image.getSize stripped) <| \sizeFile ->
             Do.withFile sizeFile parseSizeFile <| \sizeData ->
             Do.each standardFormats.list (convertAndResize ( stripped, originalExtension ) sizeData) <| \converted ->
             Cache.succeed
@@ -379,7 +380,7 @@ processFile config total index ( path, copyFile ) =
 
         Just "svg" ->
             Cache.do copyFile <| \hash ->
-            Do.withFile hash getSvgSize <| \size ->
+            Cache.do (Image.getSvgSize hash) <| \size ->
             Cache.succeed
                 (Just
                     (ProcessedSvg
@@ -408,68 +409,13 @@ processFile config total index ( path, copyFile ) =
             Cache.succeed (Just (ProcessedCss { filename = relative, hash = hash }))
 
         _ ->
+            -- Cache.fail ("Don't know how to process " ++ Path.toString path)
             Cache.succeed Nothing
-     -- Cache.fail ("Don't know how to process " ++ Path.toString path)
     )
         |> Cache.timed
             ("Processing " ++ Path.toString path)
             ("Processed  " ++ Path.toString path)
         |> Cache.withPrefix prefix
-
-
-getSvgSize : String -> Cache.Monad { width : Int, height : Int }
-getSvgSize input =
-    case Parser.run viewBoxParser input of
-        Ok { width, height } ->
-            Cache.succeed { width = width, height = height }
-
-        Err e ->
-            Cache.fail (errorToString input e)
-
-
-viewBoxParser : Parser { x : Int, y : Int, width : Int, height : Int }
-viewBoxParser =
-    Parser.succeed (\x y w h -> { x = x, y = y, width = w, height = h })
-        |. doctypeParser
-        |. Parser.spaces
-        |. Parser.symbol "<svg"
-        |. Parser.Workaround.chompUntilAfter "viewBox=\""
-        |= Parser.int
-        |. Parser.spaces
-        |= Parser.int
-        |. Parser.spaces
-        |= Parser.int
-        |. Parser.spaces
-        |= Parser.int
-        |. Parser.symbol "\""
-
-
-doctypeParser : Parser ()
-doctypeParser =
-    Parser.oneOf
-        [ Parser.succeed ()
-            |. Parser.symbol "<?xml"
-            |. Parser.Workaround.chompUntilAfter "?>"
-        , Parser.succeed ()
-        ]
-
-
-errorToString :
-    String
-    -> List DeadEnd
-    -> String
-errorToString src deadEnds =
-    Parser.Error.renderError
-        { text = identity
-        , formatContext = Ansi.Color.fontColor Ansi.Color.cyan
-        , formatCaret = Ansi.Color.fontColor Ansi.Color.red
-        , newline = "\n"
-        , linesOfExtraContext = 3
-        }
-        Parser.Error.forParser
-        src
-        deadEnds
-        |> String.concat
 
 
 minSize : number
---
+++ src/Cache/Image.elm
@@ -0,0 +1,77 @@
+module Cache.Image exposing (doctypeParser, errorToString, getSize, getSvgSize, stripMetadata, viewBoxParser)
+
+import Ansi.Color
+import Cache exposing (FileOrDirectory)
+import Parser exposing ((|.), (|=), Parser)
+import Parser.Error
+import Parser.Workaround
+
+
+getSvgSize : FileOrDirectory -> Cache.Monad { width : Int, height : Int }
+getSvgSize hash =
+    Cache.withFile hash parseSvgSize
+
+
+parseSvgSize : String -> Cache.Monad { width : Int, height : Int }
+parseSvgSize input =
+    case Parser.run viewBoxParser input of
+        Ok { width, height } ->
+            Cache.succeed { width = width, height = height }
+
+        Err e ->
+            Cache.fail (errorToString input e)
+
+
+viewBoxParser : Parser { x : Int, y : Int, width : Int, height : Int }
+viewBoxParser =
+    Parser.succeed (\x y w h -> { x = x, y = y, width = w, height = h })
+        |. doctypeParser
+        |. Parser.spaces
+        |. Parser.symbol "<svg"
+        |. Parser.Workaround.chompUntilAfter "viewBox=\""
+        |= Parser.int
+        |. Parser.spaces
+        |= Parser.int
+        |. Parser.spaces
+        |= Parser.int
+        |. Parser.spaces
+        |= Parser.int
+        |. Parser.symbol "\""
+
+
+doctypeParser : Parser ()
+doctypeParser =
+    Parser.oneOf
+        [ Parser.succeed ()
+            |. Parser.symbol "<?xml"
+            |. Parser.Workaround.chompUntilAfter "?>"
+        , Parser.succeed ()
+        ]
+
+
+errorToString :
+    String
+    -> List Parser.DeadEnd
+    -> String
+errorToString src deadEnds =
+    Parser.Error.renderError
+        { text = identity
+        , formatContext = Ansi.Color.fontColor Ansi.Color.cyan
+        , formatCaret = Ansi.Color.fontColor Ansi.Color.red
+        , newline = "\n"
+        , linesOfExtraContext = 3
+        }
+        Parser.Error.forParser
+        src
+        deadEnds
+        |> String.concat
+
+
+stripMetadata : FileOrDirectory -> Cache.Monad FileOrDirectory
+stripMetadata hash =
+    Cache.pipeThrough "exiftool" [ "-all=", "-", "-o", "-" ] hash
+
+
+getSize : FileOrDirectory -> Cache.Monad FileOrDirectory
+getSize file =
+    Cache.pipeThrough "identify" [ "-ping", "-format", "%w %h", "-" ] file
