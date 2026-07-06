cf5c4e02ae0eaf075893a7aeaa3d9cc85704deec
Leonardo Taglialegne <leonardo@taglialegne.it>
2026-07-06 20:38:37 +0200

    Warn if the result of running elm-format is empty

--- src/BuildTask/Elm.elm
+++ src/BuildTask/Elm.elm
@@ -3,6 +3,7 @@ module BuildTask.Elm exposing (codegen, format)
 import BuildTask exposing (BuildTask, FileOrDirectory)
 import BuildTask.Do as Do
 import BuildTask.Unsafe as Unsafe
+import Hash
 import Path exposing (Path)
 
 
@@ -17,13 +18,30 @@ codegen :
     }
     -> BuildTask { filename : Path, hash : FileOrDirectory }
 codegen file =
-    Do.writeFile file.contents <| \hash ->
-    BuildTask.do (format hash) <| \formatted ->
-    { filename = Path.path ("generated/" ++ file.path)
-    , hash = formatted
-    }
-        |> BuildTask.succeed
-        |> BuildTask.withWarnings (List.map formatWarning file.warnings)
+    if String.isEmpty (String.trim file.contents) then
+        if List.isEmpty file.warnings then
+            BuildTask.fail ("Will not write an empty file at " ++ file.path)
+
+        else
+            BuildTask.fail ("Will not write an empty file at " ++ file.path ++ "\nWarnings were:" ++ String.concat (List.map (\w -> "- " ++ formatWarning w) file.warnings))
+
+    else
+        Do.writeFile file.contents <| \hash ->
+        BuildTask.do (format hash) <| \formatted ->
+        Do.withFile formatted
+            (\content ->
+                if String.isEmpty (String.trim content) then
+                    BuildTask.fail ("elm-format produced an empty file - input was " ++ Hash.toString hash)
+
+                else
+                    BuildTask.succeed ()
+            )
+        <| \() ->
+        { filename = Path.path ("generated/" ++ file.path)
+        , hash = formatted
+        }
+            |> BuildTask.succeed
+            |> BuildTask.withWarnings (List.map formatWarning file.warnings)
 
 
 formatWarning : { declaration : String, warning : String } -> BuildTask.Warning
